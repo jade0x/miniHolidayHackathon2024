f5d658e021e937758dadcffeda92182aec420140
winx33 <119149884+winx33@users.noreply.github.com>
2024-12-15 08:41:12 -0600

    Menu, visit counter, styling

--- src/index.js
+++ src/index.js
@@ -3,3 +3,20 @@ function changeBG() {
     ? document.body.classList.remove("alt")
     : document.body.classList.add("alt");
 }
+
+async function getVisitCount() {
+  try {
+    const response = await fetch(
+      "https://api.countapi.xyz/hit/holidayhack2024.netlify.app//visits"
+    );
+    const data = await response.json();
+    document.getElementById(
+      "visit-count"
+    ).innerText = `This page has been visited ${data.value} times`;
+  } catch (error) {
+    console.error("Error fetching count:", error);
+    document.getElementById("visit-count").innerText = "Error loading";
+  }
+}
+
+getVisitCount();
